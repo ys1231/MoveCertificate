7ebf537af36c9b1ee1c5d2038cc4076fdda5c465
iyueforlinux <ys1231@126.com>
2024-02-23 17:17:26 +0800

    fix(sh): :bug: #10 conscrypt other version Add excute log

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -13,34 +13,69 @@ sdk_version=$(getprop ro.build.version.sdk)
 #sdk_version=34
 sdk_version_number=$(expr "$sdk_version" + 0)
 
+# add logcat
+LOG_PATH="$MODDIR/install.log"
+LOG_TAG="iyue_MoveCertificate"
+
+# Keep only one up-to-date log
+echo "[$LOG_TAG] Keep only one up-to-date log" >$LOG_PATH
+
+print_log() {
+    echo "[$LOG_TAG] $@" >>$LOG_PATH
+}
+
+mount_cert() {
+    # "Mount a temporary directory to overwrite the system certificate directory"
+    print_log "mount: $1"
+    mount -t tmpfs tmpfs "$1"
+    print_log "mount status:$?"
+    # "Copy all certificates to the system certificate directory"
+    print_log "move cert: $1"
+    cp -f $MODDIR/certificates/* "$1"
+    chown -R system:system "$1"
+    chown root:shell "$1"
+    chmod -R 644 "$1"
+    chmod 755 "$1"
+    print_log "move cert status:$?"
+}
+
+print_log "start move cert !"
+print_log "current sdk version is $sdk_version_number"
+
+# Android version >= 14 execute
 if [ "$sdk_version_number" -ge 34 ]; then
+
     chown -R root:root $MODDIR/certificates/
     chmod -R 644 $MODDIR/certificates
     chmod 755 $MODDIR/certificates
-    # Copy out the existing certificates
+
+    print_log "Backup system certificates"
     cp -u /system/etc/security/cacerts/* $MODDIR/certificates/
-    # Move our new certificate, so we trust that too
+    cp -u /apex/com.android.conscrypt/cacerts/* $MODDIR/certificates/
+
+    print_log "Backup user certificates"
     cp -u /data/misc/user/0/cacerts-added/* $MODDIR/certificates/
-    # Create the in-memory mount on top of the system certs folder
-    mount -t tmpfs tmpfs /apex/com.android.conscrypt/cacerts/
-    # Copy the existing certs back into the tmpfs, so we keep trusting them
-    cp -f $MODDIR/certificates/* /apex/com.android.conscrypt/cacerts/
-    # Update the perms & selinux context labels
-    chown -R system:system /apex/com.android.conscrypt/cacerts/
-    chown root:shell /apex/com.android.conscrypt/cacerts
-    chmod -R 644 /apex/com.android.conscrypt/cacerts/
-    chmod 755 /apex/com.android.conscrypt/cacerts/
-else
-    cp -u /system/etc/security/cacerts/* $MODDIR/certificates
-    cp -u /data/misc/user/0/cacerts-added/* $MODDIR/certificates/
-    chown -R root:root $MODDIR/certificates/
-    chmod -R 644 $MODDIR/certificates
-    chmod 755 $MODDIR/certificates
-    mount -t tmpfs tmpfs /system/etc/security/cacerts/
-    cp -f $MODDIR/certificates/* /system/etc/security/cacerts/
-    chown root:root /system/etc/security/cacerts
-    chown -R root:root /system/etc/security/cacerts/
-    chmod -R 644 /system/etc/security/cacerts/
-    chmod 755 /system/etc/security/cacerts
-    chcon u:object_r:system_file:s0 /system/etc/security/cacerts/*
+
+    print_log "find system conscrypt directory"
+    apex_dir=$(find /apex -type d -name "com.android.conscrypt@*")
+    print_log "find conscrypt directory: $apex_dir"
+    mount_cert "$apex_dir/cacerts/"
+    mount_cert /apex/com.android.conscrypt/cacerts/
+
 fi
+
+# All Android versions perform
+cp -u /system/etc/security/cacerts/* $MODDIR/certificates
+cp -u /data/misc/user/0/cacerts-added/* $MODDIR/certificates/
+chown -R root:root $MODDIR/certificates/
+chmod -R 644 $MODDIR/certificates
+chmod 755 $MODDIR/certificates
+mount -t tmpfs tmpfs /system/etc/security/cacerts/
+cp -f $MODDIR/certificates/* /system/etc/security/cacerts/
+chown root:root /system/etc/security/cacerts
+chown -R root:root /system/etc/security/cacerts/
+chmod -R 644 /system/etc/security/cacerts/
+chmod 755 /system/etc/security/cacerts
+chcon u:object_r:system_file:s0 /system/etc/security/cacerts/*
+print_log "exit status:$?"
+print_log "certificates installed"
